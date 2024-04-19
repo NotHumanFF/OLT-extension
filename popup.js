95b32275c1297c3d70ecb94a43005a7b3f31cbf1
Parth Awasthi <107567458+NotHumanFF@users.noreply.github.com>
2024-04-19 14:23:48 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,34 @@
+document.getElementById('run-button').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0];
+      chrome.scripting.executeScript({
+        target: { tabId: activeTab.id },
+        function: executeCodeOnWebsite
+      });
+    });
+  });
+  
+  function executeCodeOnWebsite() {
+      let arr = window.parent.a.secQuesMap[1];
+      let ans = [];
+      arr.forEach(elem => {
+          if (elem[3] === elem[10][0])
+              ans.push("A");
+          else if (elem[3] === elem[11][0])
+              ans.push("B");
+          else if (elem[3] === elem[12][0])
+              ans.push("C");
+          else if (elem[3] === elem[13][0])
+              ans.push("D");
+      });
+      let i = 1;
+      ans.forEach(elem => {
+          // Send console messages to the extension popup
+          chrome.runtime.sendMessage({
+            action: 'log',
+            data: i + " : " + elem
+          });
+          i++;
+      });
+  }
+  
